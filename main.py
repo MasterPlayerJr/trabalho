6d005157ff59aa129e2576a5d496c51e91cf17b6
MasterPlayerJr <evaldomasterjr@hotmail.com>
2022-10-24 20:32:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-import create, list, list_id, edit, delete, calc_mean, menu, functions
+from menu import menu
+from create import criar
 
+NOME_DATABASE = "database.csv"
+alunos = []
+
+def salvar_arq():
+    with open(NOME_DATABASE,"w") as arq:
+        sep = arq.readline() # Lendo primeira linhas
+        variaveis = arq.readline() # Lendo segunda linha  
+        arq.writelines((sep,"\n"))
+        arq.writelines((variaveis,"\n"))
+        for i in range((len(arq.readlines()))):
+            aluno = ';'.join(alunos[i])
+            arq.writelines((aluno,"\n"))
+            
+    
 def main():
-    if file
+    escolha = menu()
+    if escolha == "+":
+        aluno = criar()
+        
+salvar_arq()
