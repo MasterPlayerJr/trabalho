f1a9c623358224c7aa3bf450be2b138d3872dccd
MasterPlayerJr <evaldomasterjr@hotmail.com>
2022-10-24 20:32:10 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,4 +1,8 @@
-import create, list, list_id, edit, delete, calc_mean, menu, functions
+from functions import limpar
 
-def start():
+def menu():
+    limpar(0)
+    print(" == Matricula == ")
     print("")
+    escolha = input("")
+    return escolha
