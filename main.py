63fe75cf5dcb8a4ebd75bf2eb668df9b1a42e3c7
MasterPlayerJr <evaldomasterjr@hotmail.com>
2022-10-25 20:35:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 from menu import menu
 from create import criar
+from functions import lista_str
+from delete import deletar
+from list import ler_alunos
+from env_ambient import ultimo_id,NOME_DATABASE
 
-NOME_DATABASE = "database.csv"
-alunos = []
+alunos = ler_alunos()
 
 def salvar_arq():
-    with open(NOME_DATABASE,"w") as arq:
-        sep = arq.readline() # Lendo primeira linhas
-        variaveis = arq.readline() # Lendo segunda linha  
-        arq.writelines((sep,"\n"))
-        arq.writelines((variaveis,"\n"))
-        for i in range((len(arq.readlines()))):
-            aluno = ';'.join(alunos[i])
-            arq.writelines((aluno,"\n"))
-            
-    
+    with open(NOME_DATABASE,"a") as arq:
+        for i in range(0,ultimo_id):
+            try:
+                alunos_str = lista_str(alunos[i])
+                alunos_str = ";".join(alunos_str)
+                arq.writelines(f"\n{alunos_str}")
+            except IndexError:
+                continue
+       
 def main():
     escolha = menu()
-    if escolha == "+":
+    if escolha == "1":
         aluno = criar()
-        
-salvar_arq()
+        alunos.append(aluno)
+    elif escolha == "3":
+        deletar()
+
+while True:
+    main()
+    salvar_arq()
