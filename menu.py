0ec281639c46dd8c4f26df139c5654b35915884c
MasterPlayerJr <evaldomasterjr@hotmail.com>
2022-10-25 20:35:15 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -2,7 +2,14 @@ from functions import limpar
 
 def menu():
     limpar(0)
-    print(" == Matricula == ")
-    print("")
-    escolha = input("")
-    return escolha
+    while True:
+        print(" == Matricula == ")
+        print("1 - Cadastro de Aluno")
+        print("2 - Listar Todos os Alunos")
+        print("3 - Listar Aluno em Especifico")
+        print("4 - Atualizar Aluno")
+        print("5 - Excluir Aluno")
+        print("6 - Media do Aluno")
+        print("0 - Sair")
+        escolha = input("")
+        return escolha 
