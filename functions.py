7cedb16781966d75a8687abc68063acbd17d7950
MasterPlayerJr <evaldomasterjr@hotmail.com>
2022-10-25 20:35:03 -0300

    added

--- functions.py
+++ functions.py
@@ -4,3 +4,8 @@ from time import sleep
 def limpar(tempo):
     sleep(tempo)
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def lista_str(lista):
+    for i in range(len(lista)):
+        lista[i] = str(lista[i])
+    return lista
