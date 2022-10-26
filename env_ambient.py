f44dee89f0634489701f53ceef20486ee7a567c8
MasterPlayerJr <evaldomasterjr@hotmail.com>
2022-10-26 20:37:53 -0300

    Update env_ambient.py

--- env_ambient.py
+++ env_ambient.py
@@ -2,19 +2,3 @@ NOME_DATABASE = "database.csv"
 NOME_ARQ_ID = "ultimo_id.txt"
 with open(NOME_ARQ_ID,'r') as arq:
     ultimo_id = int(arq.read())
-    
-def ler_alunos():
-    alunos = []
-    with open(NOME_DATABASE,'r+') as arq:
-        arq.readline()
-        arq.readline()
-        for i in range(0,ultimo_id):
-            aluno = arq.readline().split(";")
-            if len(aluno) > 5:
-                aluno.pop()
-            alunos.append(aluno)
-            print(alunos)
-            input()
-    return alunos
-
-alunos = ler_alunos()
