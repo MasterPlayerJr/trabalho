085457b11f2364fea7d0c53d170a713cc01f265f
MasterPlayerJr <evaldomasterjr@hotmail.com>
2022-10-26 19:40:35 -0300

    save

--- delete.py
+++ delete.py
@@ -1,5 +1,14 @@
+from env_ambient import alunos
+from functions import limpar
 
 def deletar():
-    print(" == Deletar Aluno ==")
-    escolha_id = int(input("Escolha o id a ser deletado:"))
-    
+    fechar = False
+    while not fechar:
+        limpar(0)
+        print(" == Deletar Aluno ==")
+        escolha_id = input("Escolha o id a ser deletado:")
+        for i in range(len(alunos)):
+            if alunos[i][0] == escolha_id:
+                alunos.pop(i)
+                fechar = True
+    return alunos
--- main.py
+++ main.py
@@ -2,29 +2,39 @@ from menu import menu
 from create import criar
 from functions import lista_str
 from delete import deletar
-from list import ler_alunos
-from env_ambient import ultimo_id,NOME_DATABASE
+from env_ambient import ultimo_id,NOME_DATABASE,alunos
 
-alunos = ler_alunos()
-
-def salvar_arq():
-    with open(NOME_DATABASE,"a") as arq:
-        for i in range(0,ultimo_id):
+def salvar_arq(alunos_save):
+    with open(NOME_DATABASE,"w") as arq:
+        arq.writelines("sep=; \n")
+        arq.writelines("id;nome;matricula;idade;nota")
+        for i in range(0,ultimo_id + 1):
             try:
-                alunos_str = lista_str(alunos[i])
+                alunos_str = lista_str(alunos_save[i])
                 alunos_str = ";".join(alunos_str)
-                arq.writelines(f"\n{alunos_str}")
+                arq.writelines(f"\n{alunos_str};")
+                print("Deu")
+                input()
             except IndexError:
+                print("Erro")
+                input()
                 continue
+
        
 def main():
+    global alunos
     escolha = menu()
     if escolha == "1":
         aluno = criar()
         alunos.append(aluno)
-    elif escolha == "3":
-        deletar()
+        print(alunos)
+        input()
+        salvar_arq(alunos)
+    elif escolha == "5":
+        alunos = deletar()
+        salvar_arq(alunos)
+    elif escolha == "s":
+        salvar_arq(alunos)
 
 while True:
     main()
-    salvar_arq()
