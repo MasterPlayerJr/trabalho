e4cf3595d52a154452161cab745e08527c200ef0
MasterPlayerJr <evaldomasterjr@hotmail.com>
2022-11-03 20:46:07 -0300

    Update edit.py

--- edit.py
+++ edit.py
@@ -1,18 +1,14 @@
 from env_ambient import quant_linhas
-from functions import ler_alunos
+from functions import ler_alunos, salvar_arq
 from random import randint
 
 def editar():
     alunos = ler_alunos()
     escolha = int(input("Escolha o id do aluno a editar:"))
-    for i in range(0,quant_linhas):
+    for i in range(0,quant_linhas - 2):
         if int(alunos[i][0]) == escolha:
-            aluno = alunos.pop(i)
-            aluno[1] = input("Escreva o nome:")
-            aluno[2] = randint(1,100000)
-            aluno[3] = input("Informe a idade:")
-            aluno[4] = input("Escreva a nova nota:")
-
-            
-
-editar()
+            alunos[i][1] = input("Escreva o nome:")
+            alunos[i][2] = randint(1,100000)
+            alunos[i][3] = input("Informe a idade:")
+            alunos[i][4] = input("Escreva a nova nota:")
+    salvar_arq(alunos)
